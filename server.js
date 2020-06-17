f74f771a7aea2cf7db04a3c985f35b5b00b854c3
Hoyeon Kim <kim.hoyeon93@gmail.com>
2020-06-16 23:40:46 -0700

    deployed in heroku with mLab.

--- server.js
+++ server.js
@@ -1,28 +1,20 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
-
-const PORT = 3000;
-
+require('dotenv').config();
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const compression = require('compression');
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
-
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
+app.use(express.static('public'));
 // routes
-app.use(require("./routes/api.js"));
+app.use(require('./routes/api.js'));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+require('./config')
+  .then(() => app.listen(process.env.PORT || 3000))
+  .catch((err) => console.error(err));
